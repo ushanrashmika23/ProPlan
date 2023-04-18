046b53f6a6d6c8da4e48c33139a8dffc12a359ff
Ushan Rashmika <ushanrashmika23@gmail.com>
2023-04-18 16:22:03 +0530

    color test remove

--- ajax/refresh-pro-cards.php
+++ ajax/refresh-pro-cards.php
@@ -7,7 +7,5 @@ $res = mysqli_query($conn, $sql);
 
 while ($data = mysqli_fetch_assoc($res)) {
     //print_r($data);
-    $rand = str_pad(dechex(rand(0x000000, 0xFFFFFF)), 6, 0, STR_PAD_LEFT);
-    $color='#' . $rand;
     echo " <div class='pro-card'><label>#{$data['id']}</label><p>{$data['name']}</p></div><!-- pro-card -->";
 }
